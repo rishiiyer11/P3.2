03a696966897c62e237778e8f44f4d20e518ce83
Rishi Iyer <rishiiyer11@gmail.com>
2025-06-04 23:43:21 -0700

    Phase 2

--- libfs/fs.c
+++ libfs/fs.c
@@ -10,6 +10,9 @@
 // def 
 #define BLOCK_SIZE 4096
 #define FS_SIGNATURE "ECS150FS"
+#define FAT_EOC 0xFFFF
+#define MAX_FILENAME 16
+#define MAX_FILE_COUNT 128
 
 // glob
 static struct superblock *super = NULL;
@@ -42,24 +45,20 @@ struct __attribute__((packed)) root {
 int fs_mount(const char *disk)
 {
 	if (mount) {
-        fprintf(stderr, "file sys mounted\n");
         return -1;
     }
 
     if (block_disk_open(disk) < 0) {
-        fprintf(stderr, "can't open disk %s\n", disk);
         return -1;
     }
 
     super = malloc(sizeof(struct superblock));
     if (!super) {
-        fprintf(stderr, "failed malloc for super\n");
         block_disk_close();
         return -1;
     }
 
     if (block_read(0, super) < 0) {
-        fprintf(stderr, "failed read for super\n");
         free(super);
         super = NULL;
         block_disk_close();
@@ -67,7 +66,6 @@ int fs_mount(const char *disk)
     }
 
     if (memcmp(super->sig, FS_SIGNATURE, 8) != 0) {
-        fprintf(stderr, "inv file sys sig\n");
         free(super);
         super = NULL;
         block_disk_close();
@@ -76,7 +74,6 @@ int fs_mount(const char *disk)
 
     int blckCnt = block_disk_count();
     if (blckCnt < 0 || super->totBlck != blckCnt) {
-        fprintf(stderr, "blck cnt mismatch\n");
         free(super);
         super = NULL;
         block_disk_close();
@@ -86,7 +83,6 @@ int fs_mount(const char *disk)
     size_t fatSize = super->fatBlckCnt * BLOCK_SIZE;
     fat = malloc(fatSize);
     if (!fat) {
-        fprintf(stderr, "failed malloc for FAT\n");
         free(super);
         super = NULL;
         block_disk_close();
@@ -95,7 +91,6 @@ int fs_mount(const char *disk)
 
     for (int i = 0; i < super->fatBlckCnt; i++) {
         if (block_read(i + 1, (uint8_t*)fat + i * BLOCK_SIZE) < 0) {
-            fprintf(stderr, "failed read for FAT\n");
             free(fat);
             fat = NULL;
             free(super);
@@ -107,7 +102,6 @@ int fs_mount(const char *disk)
 
     rdir = malloc(sizeof(struct root));
     if (!rdir) {
-        fprintf(stderr, "failed malloc for root\n");
         free(fat);
         fat = NULL;
         free(super);
@@ -117,7 +111,6 @@ int fs_mount(const char *disk)
     }
 
     if (block_read(super->rootInd, rdir) < 0) {
-        fprintf(stderr, " failed read for root\n");
         free(rdir);
         rdir = NULL;
         free(fat);
@@ -135,7 +128,18 @@ int fs_mount(const char *disk)
 int fs_umount(void)
 {
 	if (!mount) {
-        fprintf(stderr, "no file sys mounted\n");
+        return -1;
+    }
+
+    // write FAT to disk
+    for (int i = 0; i < super->fatBlckCnt; i++) {
+        if (block_write(i + 1, (uint8_t*)fat + i * BLOCK_SIZE) < 0) {
+            return -1;
+        }
+    }
+
+    // write root to disk
+    if (block_write(super->rootInd, rdir) < 0) {
         return -1;
     }
 
@@ -147,7 +151,6 @@ int fs_umount(void)
     super = NULL;
 
     if (block_disk_close() < 0) {
-        fprintf(stderr, "failed close disk\n");
         return -1;
     }
 
@@ -158,7 +161,6 @@ int fs_umount(void)
 int fs_info(void)
 {
 	if (!mount) {
-        fprintf(stderr, "no file sys mounted\n");
         return -1;
     }
 
@@ -176,7 +178,7 @@ int fs_info(void)
         }
     }
 
-    // print info (EDIT TO MAKE FIT REF PRGRM)
+    // print info
     printf("totBlckCnt=%d\n", super->totBlck);
     printf("fatBlckCnt=%d\n", super->fatBlckCnt);
     printf("rdirBlck=%d\n", super->rootInd);
@@ -184,6 +186,116 @@ int fs_info(void)
     printf("dataBlckCnt=%d\n", super->dataBlckCnt);
     printf("fat/free=%d/%d\n", free_blocks, super->dataBlckCnt);
     printf("rdir/free=%d/%d\n", 128 - file_count, 128);
+
+    return 0;
+}
+
+int fs_create(const char *filename)
+{
+    if (!mount) {
+        return -1;
+    }
+
+    if (!filename) {
+        return -1;
+    }
+
+    // check filename
+    if (strlen(filename) >= MAX_FILENAME) {
+        return -1;
+    }
+
+    // check filename empty
+    if (strlen(filename) == 0) {
+        return -1;
+    }
+
+    // check file exists
+    for (int i = 0; i < MAX_FILE_COUNT; i++) {
+        if (rdir->ent[i].fileName[0] != '\0' && 
+            strcmp((char*)rdir->ent[i].fileName, filename) == 0) {
+            return -1;
+        }
+    }
+
+    // find empty entry in root
+    int emptyEnt = -1;
+    for (int i = 0; i < MAX_FILE_COUNT; i++) {
+        if (rdir->ent[i].fileName[0] == '\0') {
+            emptyEnt = i;
+            break;
+        }
+    }
+
+    if (emptyEnt == -1) {
+        return -1;
+    }
+
+    // init file entry
+    memset(&rdir->ent[emptyEnt], 0, sizeof(struct FAT));
+    strcpy((char*)rdir->ent[emptyEnt].fileName, filename);
+    rdir->ent[emptyEnt].fileSize = 0;
+    rdir->ent[emptyEnt].firstBlck = FAT_EOC;
+
+    return 0;
+}
+
+int fs_delete(const char *filename)
+{
+    if (!mount) {
+        return -1;
+    }
+
+    if (!filename) {
+        return -1;
+    }
+
+    // find file in root
+    int fileEnt = -1;
+    for (int i = 0; i < MAX_FILE_COUNT; i++) {
+        if (rdir->ent[i].fileName[0] != '\0' && 
+            strcmp((char*)rdir->ent[i].fileName, filename) == 0) {
+            fileEnt = i;
+            break;
+        }
+    }
+
+    if (fileEnt == -1) {
+        return -1;
+    }
+
+    // free FAT data blocks
+    uint16_t currBlck = rdir->ent[fileEnt].firstBlck;
+    while (currBlck != FAT_EOC && currBlck != 0) {
+        uint16_t nextBlck = fat[currBlck];
+        
+        // mark free
+        fat[currBlck] = 0;
+        currBlck = nextBlck;
+    }
+
+    // clear file entry in root
+    memset(&rdir->ent[fileEnt], 0, sizeof(struct FAT));
+
+    return 0;
+}
+
+int fs_ls(void)
+{
+    if (!mount) {
+        return -1;
+    }
+
+    printf("FS Ls:\n");
+    
+    for (int i = 0; i < MAX_FILE_COUNT; i++) {
+        if (rdir->ent[i].fileName[0] != '\0') {
+            printf("file: %s, size: %u, data_blk: %u\n",
+                   rdir->ent[i].fileName,
+                   rdir->ent[i].fileSize,
+                   rdir->ent[i].firstBlck);
+        }
+    }
 
     return 0;
 }
